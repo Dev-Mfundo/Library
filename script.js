ec058b763f014ec784641e5035bdf4dfcfd7b1e8
devmfundo <devmfundo@gmail.com>
2024-08-15 14:56:53 +0200

    javascript: console can output book values which is half the job.

--- script.js
+++ script.js
@@ -14,21 +14,48 @@ cancelBtn.addEventListener("click", hideForm=()=>{
   form.style.display='none';
 })
 checkboxYes.addEventListener("change", ()=>{
-  if(checkboxYes.checked){
+  if(checkboxYes.checked==true){
     checkboxNo.checked=false;
   }
 })
 checkboxNo.addEventListener("change", ()=>{
-  if(checkboxNo.checked){
+  if(checkboxNo.checked==true){
     checkboxYes.checked=false;
   }
 })
+
+const addBookNow=document.getElementById("addBook");
+addBookNow.addEventListener("click", ()=>{
+
+const author = document.getElementById("author").value;
+const title = document.getElementById("title").value;
+const pages = document.getElementById("pages").value;
+
+let read =()=>{
+  if(checkboxNo.checked){
+    console.log("No");
+    return "Not yet!";
+  }else if(checkboxYes.checked){
+    console.log("yes");
+    return "Read";
+  }
+}
 const myLibrary = [];
 
-function Book() {
+function Book(author,title,pages,read) {
+  this.author=author,
+  this.title=title,
+  this.pages=pages,
+  this.read=read
 
 }
+const newBookInfo= new Book({author},{title},{pages},{read});
+
+console.log(newBookInfo);
+
 
 function addBookToLibrary() {
 
 }
+
+})
